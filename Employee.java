28570de44816473388d532c3d047f3d73d1dc308
kesavan99 <81011748+kesavan99@users.noreply.github.com>
2021-07-09 14:23:25 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,71 +1,35 @@
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
-class Employee
+class Employee{
+  private String name, address;
+  private int year, salary;
+  public Employee(String n, int y, int sal, String add){
+    name = n;
+    year = y;
+    salary = sal;
+    address = add;
+  }
+  public String getName(){
+    return name;
+  }
+  public int getYear(){
+    return year;
+  }
+  public int getSalary(){
+    return salary;
+  }
+  public String getAddress(){
+    return address;
+  }
 
-{
-    double salary; 
-int hours;
-
-
-public void getInfo()
-
-{
-Scanner in = new Scanner(System.in);
-
-
-System.out.println("Enter your salary");
-
- salary = in.nextDouble();
-
-System.out.println("Enter the no.of working hours");
-
- hours = in.nextInt();
-
+  public static void main(String[] args){
+    Employee e1 = new Employee("Robert", 1994, 500000, "64C- WallsStreet");
+    Employee e2 = new Employee("Sam", 2000, 740000, "68d- WallsStreet");
+    Employee e3 = new Employee("John", 1999, 600000, "26B- WallsStreet");
+    System.out.println("Name\tYear of joining\tSalary\tAddress");
+    System.out.println(e1.getName()+"\t"+e1.getYear()+"\t\t\t"+e1.getSalary()+"\t"+e1.getAddress());  // printing details of employee 1
+    System.out.println(e2.getName()+"\t"+e2.getYear()+"\t\t\t"+e2.getSalary()+"\t"+e2.getAddress());  // printing details of employee 2
+    System.out.println(e3.getName()+"\t"+e3.getYear()+"\t\t\t"+e3.getSalary()+"\t"+e3.getAddress());  // printing details of employee 3
+  }
 }
-
-
-public void AddSal()
-
-{
-
-
-if(salary < 500)
-
-salary = salary + 10;
-
-System.out.println("Salary: " + salary);
-
-}
-
-public void AddWork()
-
-{
-
-
-if(hours > 6)
-
-salary =salary + 5;
-
-System.out.println("Salary: " + salary);
-
-}
-
-public static void main(String[] args)
-
-{
-
-Scanner in = new Scanner(System.in);
-
-Employee obj = new Employee();
-
-obj.getInfo();
-
-obj.AddSal();
-
-obj.AddWork();
-
-}
-
-}
-
--- Rectangle.java
+++ Rectangle.java
@@ -1,29 +1,32 @@
 import java.io.*;
-import java.util.Scanner;
-class area{
-    int length;
-    int breadth;
-    area(int a, int b)
-    {
-         length = a;
-         breadth = b;
-    }
-    public int returnarea()
-    {
-        return length * breadth;
-    }
-}
+import java.util.*;
 
-public class Rectangle {
-    public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
-        int a,b;
-        System.out.println("Enter the lenght of Recatangle");
-         a = obj.nextInt();
-        obj.nextLine();
-        System.out.println("Enter the breadth of Recatangle");
-        b = obj.nextInt();
-        area ob1 = new area(a,b);
-        System.out.println("Area = "+ ob1.returnarea());
+public class Rectangle{
+    int lenght;
+    int breath;
+
+    Rectangle(int l, int b) {
+        this.lenght = l;
+        this.breath = b;
     }
+
+    public int area()
+    {
+        return lenght * breath;
+    }
+
+    public static void main(String[] args) {
+        Rectangle obj = new Rectangle(4,5);
+        Rectangle obj1 = new Rectangle(5,8);
+
+        System.out.println(obj.area());
+        System.out.println(obj1.area());
+
+
+
+
+
+    }
+
+
 }
